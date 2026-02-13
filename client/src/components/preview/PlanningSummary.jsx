9615aaaca5b8b786b96507c0cc3b5612104a5aab
Parmar Siddhrajsinh <siddhraj310@gmail.com>
2026-02-14 01:46:41 +0530

    frontend canvas

---
+++ client/src/components/preview/PlanningSummary.jsx
@@ -0,0 +1,12 @@
+const PlanningSummary = () => {
+  return (
+    <div>
+      <h2>Trip Planning Summary</h2>
+      <p>Route List</p>
+      <p>Total Budget</p>
+      <p>Media Count</p>
+    </div>
+  );
+};
+
+export default PlanningSummary;
---
+++ client/src/pages/TripPreviewPage.jsx
@@ -0,0 +1,11 @@
+import PlanningSummary from "../components/preview/PlanningSummary";
+
+const TripPreviewPage = () => {
+  return (
+    <div>
+      <PlanningSummary />
+    </div>
+  );
+};
+
+export default TripPreviewPage;
